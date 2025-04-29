ba8ce18312713677c7ea0cd86366bf82b353be1d
louka777 <louka_2@outlook.com>
2025-04-29 03:43:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 const path = require('path');
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('Un utilisateur connecté');
+
+  socket.on('sendMessage', (data) => {
+    io.emit('newMessage', data);
+  });
+
+  socket.on('flash', () => {
+    io.emit('flashPage');
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Un utilisateur déconnecté');
+  });
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Serveur lancé sur le port ${PORT}`);
+http.listen(PORT, () => {
+  console.log(`Serveur démarré sur le port ${PORT}`);
 });
